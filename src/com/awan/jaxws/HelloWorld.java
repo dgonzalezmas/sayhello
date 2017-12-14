04288ae05298490393e30275cb96939a4bf25f63
dgonzalezmas <dgonzalezmas@gmail.com>
2017-12-14 19:46:59 +0100

    again

--- src/com/awan/jaxws/HelloWorld.java
+++ src/com/awan/jaxws/HelloWorld.java
@@ -1,15 +1,12 @@
+/**
+ * HelloWorld.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
 package com.awan.jaxws;
 
-import javax.jws.WebMethod;
-import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;
-
-//Service Endpoint Interface
-@WebService
-@SOAPBinding(style = Style.RPC)
-public interface HelloWorld{
-
-	@WebMethod String getHelloWorldAsString(String name);
-
+public interface HelloWorld extends java.rmi.Remote {
+    public java.lang.String getHelloWorldAsString(java.lang.String arg0) throws java.rmi.RemoteException;
 }
